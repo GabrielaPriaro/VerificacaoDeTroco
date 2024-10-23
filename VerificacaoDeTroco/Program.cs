6468e87a4ceafc5488fc6b371876e23fdeaa927c
Gabriela <gabriela.priaro@gmail.com>
2024-10-23 14:20:37 +0100

    Adicionar arquivos de projeto.

---
+++ VerificacaoDeTroco/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace VerificacaoDeTroco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
